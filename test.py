b91fb66a99c4c331e8fd5bcfda5d10a17887d5fe
renanmgx <renanmgx@hotmail.com>
2023-07-27 00:13:51 -0300

    continuação do script na noite de 27-07-2023

--- test.py
+++ test.py
@@ -13,10 +13,15 @@ from time import sleep
 from tkinter import filedialog
 import tkinter as tk
 from tkinter import messagebox
-from PyQt6.QtWidgets import (QApplication, QCheckBox, QDialog, QPushButton, 
-QStyleFactory, QVBoxLayout, QMainWindow, QLabel, QGroupBox, QGridLayout)
+from PyQt6.QtCore import QDateTime, Qt, QTimer
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QWidget, QTableWidgetItem, QMainWindow)
 from PyQt6.QtGui import QFont, QFontDatabase
 from PyQt6.QtCore import Qt
+import atexit
 
 class Interface(QDialog,QMainWindow):
     def __init__(self, parent=None):
@@ -26,8 +31,6 @@ class Interface(QDialog,QMainWindow):
         disableWidgetsCheckBox = QCheckBox("&Disable widgets")
         
         self.setWindowTitle("Bot Whatsapp")
-
-
         
         self.createTopRightGroupBox()
 
@@ -37,66 +40,123 @@ class Interface(QDialog,QMainWindow):
         mainLayout.addWidget(self.topRightGroupBox, 1, 1)
         self.setLayout(mainLayout)
 
-    def test(self):
-        print("funciou")
-
-        
-
     def createTopRightGroupBox(self):
         self.topRightGroupBox = QGroupBox("")
 
+        bt_with = 150
+        bt_height = 50
+        
+        #botão iniciar
         self.__bt_iniciar = QPushButton("Iniciar")
         self.__bt_iniciar.setDefault(True)
         self.__bt_iniciar.clicked.connect(self.iniciar)
-        self.__bt_iniciar.setFixedSize(150,50)
+        self.__bt_iniciar.setFixedSize(bt_with,bt_height)
+
+        #botão carregar
+        self.__bt_carregar = QPushButton("Carregar Planilha")
+        self.__bt_carregar.setDefault(True)
+        self.__bt_carregar.clicked.connect(self.carregar)
+        self.__bt_carregar.setFixedSize(bt_with,bt_height)
+        self.__bt_carregar.setVisible(False)
+
+        #botão iniciar o bot
+        self.__bt_iniciar_bot = QPushButton("Começar Envio")
+        self.__bt_iniciar_bot.setDefault(True)
+        self.__bt_iniciar_bot.clicked.connect(self.test)
+        self.__bt_iniciar_bot.setFixedSize(bt_with,bt_height)
+        self.__bt_iniciar_bot.setVisible(False)
+
+        #tabela do status
+        self.__tabela = QTextEdit()
+        self.__tabela.setReadOnly(True)
+        self.__tabela.setVisible(False)
+
         
 
-        self.setGeometry(300,300,600,150)
+        #self.setGeometry(300,300,600,150)
+        #self.setMaximumHeight(100)
+        #self.setMaximumWidth(300)
 
         self.__title_infor_text = QLabel()
         font = QFont("Arial", 12, QFont.Weight.Bold)
         self.__title_infor_text.setFont(font)
+        self.__title_infor_text.setVisible(False)
         
         self.__infor_text = QLabel()
-
+        self.__infor_text.setVisible(False)
 
         layout = QVBoxLayout()
         layout.addWidget(self.__bt_iniciar)
+        layout.addWidget(self.__bt_carregar)
+        layout.addWidget(self.__bt_iniciar_bot)
+        layout.addWidget(self.__tabela)
         layout.addWidget(self.__title_infor_text)
         layout.addWidget(self.__infor_text)
         layout.addStretch(1)
         self.topRightGroupBox.setLayout(layout)
 
+    ##### metodos para teste
+    def test(self):
+        print(self.__dados[0])
+        self.__dados[0][0] = "ENVIADO!!!"
+        print(self.__dados[0])
+        self.exibir_status(self.__dados)
+        
+
+    ############################################
 
     def iniciar(self):
+        self.__bt_iniciar.setVisible(False)
+        self.__bt_carregar.setVisible(True)
+        self.__navegador = self.iniciar_whats()
+        
+    def carregar(self):
         caminho = self.procurar_arquivo()
         if caminho != "":
             if self.is_excel_file(caminho):
                 self.infor(reset=True)
-                self.__bt_iniciar.setVisible(False)
-                self.excel_load(caminho)
+                self.__bt_carregar.setVisible(False)
+                self.__dados = self.excel_load(caminho)
+                self.exibir_status(self.__dados)
+                self.__bt_iniciar_bot.setVisible(True)
             else:
                 self.infor("selecione apenas arquivos do excel",title="Erro")
         else:
             self.infor("nenhum arquivo selecionado tente novamente",title="Erro")
+ 
+    def exibir_status(self,data,reset=False):
+        if reset == True:
+            self.__tabela.setVisible(False)
+            return
+        self.setGeometry(300,300,400,300)
+        self.__tabela.setVisible(True)
+        datas = ""
+        for x in data:
+            datas += f"{x[0:3]}\n"
+        self.__tabela.setPlainText(str(datas))
+
 
     def excel_load(self,planilha):
         colunas = "A,B,C,D"
         quantidade_colunas = len(colunas.replace(",",""))
         df = pd.read_excel(planilha, sheet_name='WhatsApp', usecols=colunas, skiprows=7).values.tolist()
+        for x in df:
+            x.insert(0,"PRONTO PARA ENVIAR!!!")
+        return df
         
         
  
     def infor(self, mensagem="", title="", reset=False):
         if reset == False:
+            self.__infor_text.setVisible(True)
+            self.__title_infor_text.setVisible(True)
             self.__title_infor_text.setText(title)
             self.__infor_text.setText(mensagem)
         else:
+            self.__infor_text.setVisible(False)
+            self.__title_infor_text.setVisible(False)
             self.__title_infor_text.setText("")
             self.__infor_text.setText("")
-
-
-    
 
 
         ######## metodos apenas do bot do whats
@@ -104,9 +164,11 @@ class Interface(QDialog,QMainWindow):
         username = getpass.getuser()
         options = webdriver.ChromeOptions()
         options.add_argument(f"--user-data-dir=C:\\Users\\{username}\\AppData\\Local\\Google\\Chrome")
-        self.__navegador = webdriver.Chrome(options=options)
+        navegador = webdriver.Chrome(options=options)
         link = f"https://web.whatsapp.com/"
-        self.__navegador.get(link)
+        navegador.get(link)
+        atexit.register(navegador.quit)
+        return navegador
     
     def procurar_arquivo(self):
         root = tk.Tk()
