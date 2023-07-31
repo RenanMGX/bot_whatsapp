955bd4d17448f6bdf898de6b4ef22fb8cb9cb2e1
RenanMGX <renanmgx@hotmail.com>
2023-07-31 18:35:47 -0300

    ultima modificação no escritorio 31-07-2023

--- test.py
+++ test.py
@@ -22,6 +22,8 @@ from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
 from PyQt6.QtGui import QFont, QFontDatabase
 from PyQt6.QtCore import Qt
 import atexit
+import math
+import urllib
 
 class Interface(QDialog,QMainWindow):
     def __init__(self, parent=None):
@@ -97,10 +99,11 @@ class Interface(QDialog,QMainWindow):
 
     ##### metodos para teste
     def test(self):
-        print(self.__dados[0])
-        self.__dados[0][0] = "ENVIADO!!!"
-        print(self.__dados[0])
-        self.exibir_status(self.__dados)
+        #print(self.__dados[0])
+        #self.__dados[0][0] = "ENVIADO!!!"
+        #print(self.__dados[0])
+        #self.exibir_status(self.__dados)
+        self.enviar()
         
 
     ############################################
@@ -111,6 +114,13 @@ class Interface(QDialog,QMainWindow):
         self.__navegador = self.iniciar_whats()
         
     def carregar(self):
+        if self.is_browser_open() == False:
+            self.infor(reset=True)
+            self.infor("O navegador foi fechado tente novamente", title="Error:")
+            self.__bt_carregar.setVisible(False)
+            self.__bt_iniciar.setVisible(True)
+            return
+
         caminho = self.procurar_arquivo()
         if caminho != "":
             if self.is_excel_file(caminho):
@@ -143,9 +153,128 @@ class Interface(QDialog,QMainWindow):
         for x in df:
             x.insert(0,"PRONTO PARA ENVIAR!!!")
         return df
+    
+    def enviar(self):
+        self.infor(reset=True)
+        try:
+            self.__navegador.find_element(By.XPATH, '//*[@id="app"]/div/div/div[3]/div[1]/div/div/div[2]/div')
+            self.infor(reset=True)
+            self.infor("conecte-se ao whatsapp web antes de prosseguir", title="Error")
+            return
+        except:
+            pass
+
+        self.tratar_dados()
+        vesualiza_final = []
+        for dados in self.__dados_prontos:
+            if dados["Numero"] == None:
+                dados["Status"] == "Error"
+                print("1")
+                continue
+            if (dados["Mensagem"] == None) or (dados["Mensagem"] == "nan"):
+                dados["Status"] = "Error"
+                print("2")
+                continue
+            dados["Mensagem"] = dados["Mensagem"].encode('utf-8')
+            dados["Mensagem"] = urllib.parse.quote(dados["Mensagem"])
+            link = f'https://web.whatsapp.com/send?phone=55{dados["Numero"]}&text={dados["Mensagem"]}'
+            self.__navegador.get(link)
+            print("3")
+            contator_finalizar = 0
+            while True:
+                try:
+                    enviar = self.__navegador.find_element(By.XPATH, "//button[@data-testid='compose-btn-send']")
+                    print("4")
+                    break
+                except:
+                    pass
+                
+                try:
+                    enviar = self.__navegador.find_element(By.XPATH, '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[2]/div/button/div/div')
+                    print("5")
+                    break
+                except:
+                    pass
+                if contator_finalizar >= 5*60:
+                    enviar = False
+                    print("6")
+                else:
+                    contator_finalizar +=1
+                    print("7")
+                    sleep(1)
+            if enviar == False:
+                dados["Status"] == "Error"
+                print("8")
+                continue
+            else:
+                enviar.click()
+                print("9")
+            print("10")
+            
+            dados["Status"] = "Enviado!"
+            sleep(1)
+
+        for x in self.__dados_prontos:
+            vesualiza_final.append(list(x.values()))
+        self.exibir_status(vesualiza_final)
+        print("finalizado")
+
+                
         
         
- 
+    def tratar_dados(self):
+        self.__dados_prontos = []
+        for dados_brutos in self.__dados:
+            dados_temp = {}
+            dados_temp["Status"] = dados_brutos[0]
+            dados_temp["Nome"] = dados_brutos[1]
+            dados_temp["Numero"] = dados_brutos[2]
+            dados_temp["Arquivo"] = dados_brutos[3]
+            dados_temp["Mensagem"] = dados_brutos[4]
+
+            #Tratar Nome
+            try:
+                if math.isnan(dados_temp["Nome"]):
+                    dados_temp["Nome"] = None                    
+                else:
+                    dados_temp["Nome"] = str(dados_temp["Nome"])
+            except:
+                dados_temp["Nome"] = str(dados_temp["Nome"])
+    
+            #Tratar numero
+            try:
+                if math.isnan(dados_temp["Numero"]):
+                    dados_temp["Numero"] = None                    
+                else:
+                    dados_temp["Numero"] = str(dados_temp["Numero"])
+                    dados_temp["Numero"] = dados_temp["Numero"].replace("(", "").replace(")", "").replace(" ", "")
+                    dados_temp["Numero"] = int(dados_temp["Numero"])
+            except:
+                dados_temp["Numero"] = 9999999999999999
+
+            #Tratar Arquivo
+            try:
+                if math.isnan(dados_temp["Arquivo"]):
+                    dados_temp["Arquivo"] = None
+                else:
+                    dados_temp["Arquivo"] = str(dados_temp["Arquivo"])
+            except:
+                dados_temp["Arquivo"] = str(dados_temp["Arquivo"])
+
+            #Tratar Mensagem
+            try:
+                if math.isnan(dados_temp["Mensagem"]):
+                    dados_temp["Mensagem"] = None
+                else:
+                    dados_temp["Mensagem"] = str(dados_temp["Mensagem"])
+            except:
+                dados_temp["Mensagem"] = str(dados_temp["Mensagem"])
+
+            self.__dados_prontos.append(dados_temp)
+
+
+
+
     def infor(self, mensagem="", title="", reset=False):
         if reset == False:
             self.__infor_text.setVisible(True)
